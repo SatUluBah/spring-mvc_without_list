53c60c282a860fe873bdc95db24a40504505c82f
UlugbekSatymov <satymov.u@gmail.com>
2020-12-14 22:44:34 +0300

    without new in controller

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,10 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +14,12 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    private List<Car> list = new CarServiceImpl().getCarList();
+    private List<Car> list;
+
+    @Autowired
+    public void setList(CarService carService) {
+        this.list = carService.getCarList();
+    }
 
 
     @GetMapping(value ="/cars")
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,10 +1,11 @@
 package web.service;
 
+import org.springframework.stereotype.Repository;
 import web.model.Car;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Repository
 public class CarServiceImpl implements CarService{
 
     private List<Car> carsList;
