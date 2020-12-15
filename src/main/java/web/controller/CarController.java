30b5ebb16c368c51264e9331a02a87fea4c1110b
UlugbekSatymov <satymov.u@gmail.com>
2020-12-15 23:41:24 +0300

    without List in controller

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,30 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    private List<Car> list;
+    private CarService carService;
 
     @Autowired
-    public void setList(CarService carService) {
-        this.list = carService.getCarList();
+    public void setCarService(CarService carService) {
+        this.carService = carService;
     }
-
 
     @GetMapping(value ="/cars")
     public String showCarList(ModelMap model, @RequestParam(value = "count", defaultValue = "6") Integer count) {
         List<Car> cars = new ArrayList<>();
         if(count > 0 && count <=5) {
             for(int i = 0; i < count; i++) {
-                cars.add(list.get(i));
+                cars.add(carService.getCarList().get(i));
             }
         } else if (count > 5) {
-            cars = list;
+            cars = carService.getCarList();
         }
         model.addAttribute("cars", cars);
         return "cars";
